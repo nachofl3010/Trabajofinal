1de0e38d9b8ac052553028065900b5491c955ed6
nachofl3010 <104321914+nachofl3010@users.noreply.github.com>
2023-01-12 21:04:42 -0300

    Delete TrabajofinalCODER/tenis_tour directory

--- TrabajofinalCODER/tenis_tour/tournaments/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from tournaments.models import Tournaments
-
-# admin.site.register(Products)
-
-@admin.register(Tournaments)
-class TournamentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category')
--- TrabajofinalCODER/tenis_tour/tournaments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from tournaments.views import add_tournament, list_tournaments
-
-
-urlpatterns = [
-    path('add-tournament/', add_tournament),
-    path('list-tournaments/', list_tournaments),
-]
